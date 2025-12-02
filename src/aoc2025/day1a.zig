934fb85fb6c08a45135731f15ae1b02af3407ade
Mario-S.O <gh_mario.proclaim@slmail.me>
2025-12-02 09:40:01 +0100

    2025 day1a

--- src/aoc2025/day1a.zig
+++ src/aoc2025/day1a.zig
@@ -4,12 +4,12 @@ const print = std.debug.print;
 
 const Turn = struct {
     dir: u8,
-    amount: usize,
+    amount: i32,
 };
 
 fn parseTurn(line: []const u8) !Turn {
     const dir = line[0];
-    const amount = std.fmt.parseInt(usize, line[1..], 10) catch |err| {
+    const amount = std.fmt.parseInt(i32, line[1..], 10) catch |err| {
         return err;
     };
     return .{ .dir = dir, .amount = amount };
@@ -17,11 +17,23 @@ fn parseTurn(line: []const u8) !Turn {
 
 pub fn main() !void {
     var it = std.mem.tokenizeScalar(u8, input, '\n');
+    var currentPos: i32 = 50;
+    var zeros: usize = 0;
+
     while (it.next()) |raw_line| {
         const line = std.mem.trim(u8, raw_line, " \r\t");
         if (line.len == 0) continue;
 
         const turn = try parseTurn(line);
-        print("{c} - {d}\n", .{ turn.dir, turn.amount });
+
+        if (turn.dir == 'L') {
+            currentPos = @mod((currentPos - turn.amount), 100);
+        } else {
+            currentPos = @mod((currentPos + turn.amount), 100);
+        }
+        if (currentPos == 0) {
+            zeros += 1;
+        }
     }
+    print("Result: {d}\n", .{zeros});
 }
--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,37 @@
 const std = @import("std");
 
-const day1a15 = @import("./aoc2015/day1a.zig");
-const day1b15 = @import("./aoc2015/day1b.zig");
+const day1a15 = @import("aoc2015/day1a.zig");
+const day1b15 = @import("aoc2015/day1b.zig");
 
-const day1a16 = @import("./aoc2016/day1a.zig");
-const day1b16 = @import("./aoc2016/day1b.zig");
+const day1a16 = @import("aoc2016/day1a.zig");
+const day1b16 = @import("aoc2016/day1b.zig");
 
-const day1a17 = @import("./aoc2017/day1a.zig");
-const day1b17 = @import("./aoc2017/day1b.zig");
+const day1a17 = @import("aoc2017/day1a.zig");
+const day1b17 = @import("aoc2017/day1b.zig");
 
-const day1a18 = @import("./aoc2018/day1a.zig");
-const day1b18 = @import("./aoc2018/day1b.zig");
+const day1a18 = @import("aoc2018/day1a.zig");
+const day1b18 = @import("aoc2018/day1b.zig");
 
-const day1a19 = @import("./aoc2019/day1a.zig");
-const day1b19 = @import("./aoc2019/day1b.zig");
+const day1a19 = @import("aoc2019/day1a.zig");
+const day1b19 = @import("aoc2019/day1b.zig");
 
-const day1a20 = @import("./aoc2020/day1a.zig");
-const day1b20 = @import("./aoc2020/day1b.zig");
+const day1a20 = @import("aoc2020/day1a.zig");
+const day1b20 = @import("aoc2020/day1b.zig");
 
-const day1a21 = @import("./aoc2021/day1a.zig");
-const day1b21 = @import("./aoc2021/day1b.zig");
+const day1a21 = @import("aoc2021/day1a.zig");
+const day1b21 = @import("aoc2021/day1b.zig");
 
-const day1a22 = @import("./aoc2022/day1a.zig");
-const day1b22 = @import("./aoc2022/day1b.zig");
+const day1a22 = @import("aoc2022/day1a.zig");
+const day1b22 = @import("aoc2022/day1b.zig");
 
-const day1a23 = @import("./aoc2023/day1a.zig");
-const day1b23 = @import("./aoc2023/day1b.zig");
+const day1a23 = @import("aoc2023/day1a.zig");
+const day1b23 = @import("aoc2023/day1b.zig");
 
-const day1a24 = @import("./aoc2024/day1a.zig");
-// const day1b24 = @import("./aoc2024/day1b.zig");
+const day1a24 = @import("aoc2024/day1a.zig");
+const day1b24 = @import("aoc2024/day1b.zig");
 
-const day1a25 = @import("./aoc2025/day1a.zig");
+const day1a25 = @import("aoc2025/day1a.zig");
+const day1b25 = @import("aoc2025/day1b.zig");
 
 const DayModule = struct {
     year: u16,
@@ -58,8 +59,9 @@ const available_days = [_]DayModule{
     .{ .year = 2023, .day = "1a", .main_fn = day1a23.main },
     .{ .year = 2023, .day = "1b", .main_fn = day1b23.main },
     .{ .year = 2024, .day = "1a", .main_fn = day1a24.main },
-    // .{ .year = 2024, .day = "1b", .main_fn = day1b24.main },
+    .{ .year = 2024, .day = "1b", .main_fn = day1b24.main },
     .{ .year = 2025, .day = "1a", .main_fn = day1a25.main },
+    .{ .year = 2025, .day = "1b", .main_fn = day1b25.main },
 };
 
 pub fn main() !void {
